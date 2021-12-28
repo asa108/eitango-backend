5515c88c60cf685fb0ae19ec6cbe14a341eefa5c
Asane <yaradayo317@gmail.com>
2021-12-28 16:50:04 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ce82223fb75310a5148f96718d0a565a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '95e0c47c-3cc1-4a2a-a5a0-af61280ae74f'
+};
